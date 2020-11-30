31bf2f04b6e37e6c2c9c21ebe262607ecd369a23
Juan Ibanez <18juan.ibanez18@gmail.com>
2020-11-30 16:03:58 -0300

    update
    Crud adcionado com sucesso

--- js/script-servicos.js
+++ js/script-servicos.js
@@ -1,10 +1,145 @@
+var capturando = "";
+var capturando1 = "";
+var capturando2 = "";
+var capturando3 = "";
+var capturando4 = "";
+var capturando5 = "";
+var capturando6 = "";
+var capturando7 = "";
+var capturando8 = "";
+
+var dia = "R$ 100,00.";
+var semana = "R$ 700,00.";
+var mes = "R$ 2000,00";
+
 function modificaDia(){
-    var dia = "R$: 100";
-    
-    var modificacao = document.getElementById('modificado')
+    capturando = document.getElementById('dia').value;
+    document.getElementById('modificado').innerHTML = dia;
+}
 
-    modificacao.trim()
-    modificacao.innerText(dia);
+function modificaSemana(){
+    capturando = document.getElementById('semana').value;
+    document.getElementById('modificado').innerHTML = semana;
+}
 
-   
+function modificaMes() {
+    capturando = document.getElementById('mes').value;
+    document.getElementById('modificado').innerHTML = mes;
+}
+
+function modificaDia1(){
+    capturando1 = document.getElementById('dia1').value;
+    document.getElementById('modificado1').innerHTML = dia;
+}
+
+function modificaSemana1(){
+    capturando1 = document.getElementById('semana1').value;
+    document.getElementById('modificado1').innerHTML = semana;
+}
+
+function modificaMes1() {
+    capturando1 = document.getElementById('mes1').value;
+    document.getElementById('modificado1').innerHTML = mes;
+}
+
+function modificaDia2(){
+    capturando2 = document.getElementById('dia2').value;
+    document.getElementById('modificado2').innerHTML = dia;
+}
+
+function modificaSemana2(){
+    capturando2 = document.getElementById('semana2').value;
+    document.getElementById('modificado2').innerHTML = semana;
+}
+
+function modificaMes2() {
+    capturando2 = document.getElementById('mes2').value;
+    document.getElementById('modificado2').innerHTML = mes;
+}
+
+function modificaDia3(){
+    capturando3 = document.getElementById('dia3').value;
+    document.getElementById('modificado3').innerHTML = dia;
+}
+
+function modificaSemana3(){
+    capturando3 = document.getElementById('semana3').value;
+    document.getElementById('modificado3').innerHTML = semana;
+}
+
+function modificaMes3() {
+    capturando3 = document.getElementById('mes3').value;
+    document.getElementById('modificado3').innerHTML = mes;
+}
+
+function modificaDia4(){
+    capturando4 = document.getElementById('dia4').value;
+    document.getElementById('modificado4').innerHTML = dia;
+}
+
+function modificaSemana4(){
+    capturando4 = document.getElementById('semana4').value;
+    document.getElementById('modificado4').innerHTML = semana;
+}
+
+function modificaMes4() {
+    capturando4 = document.getElementById('mes4').value;
+    document.getElementById('modificado4').innerHTML = mes;
+}
+
+function modificaDia5(){
+    capturando5 = document.getElementById('dia5').value;
+    document.getElementById('modificado5').innerHTML = dia;
+}
+
+function modificaSemana5(){
+    capturando5 = document.getElementById('semana5').value;
+    document.getElementById('modificado5').innerHTML = semana;
+}
+
+function modificaMes5() {
+    capturando5 = document.getElementById('mes5').value;
+    document.getElementById('modificado5').innerHTML = mes;
+}
+function modificaDia6(){
+    capturando6 = document.getElementById('dia6').value;
+    document.getElementById('modificado6').innerHTML = dia;
+}
+
+function modificaSemana6(){
+    capturando6 = document.getElementById('semana6').value;
+    document.getElementById('modificado6').innerHTML = semana;
+}
+
+function modificaMes6() {
+    capturando6 = document.getElementById('mes6').value;
+    document.getElementById('modificado6').innerHTML = mes;
+}
+function modificaDia7(){
+    capturando7 = document.getElementById('dia7').value;
+    document.getElementById('modificado7').innerHTML = dia;
+}
+
+function modificaSemana7(){
+    capturando7 = document.getElementById('semana7').value;
+    document.getElementById('modificado7').innerHTML = semana;
+}
+
+function modificaMes7() {
+    capturando7 = document.getElementById('mes7').value;
+    document.getElementById('modificado7').innerHTML = mes;
+}
+function modificaDia8(){
+    capturando8 = document.getElementById('dia8').value;
+    document.getElementById('modificado8').innerHTML = dia;
+}
+
+function modificaSemana8(){
+    capturando8 = document.getElementById('semana8').value;
+    document.getElementById('modificado8').innerHTML = semana;
+}
+
+function modificaMes8() {
+    capturando8 = document.getElementById('mes8').value;
+    document.getElementById('modificado8').innerHTML = mes;
 }
