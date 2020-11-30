7e5323923ff0b6ba6219818489fbcda1c102ce33
Juan Ibanez <18juan.ibanez18@gmail.com>
2020-11-30 15:20:46 -0300

    update

---
+++ js/script-servicos.js
@@ -0,0 +1,10 @@
+function modificaDia(){
+    var dia = "R$: 100";
+    
+    var modificacao = document.getElementById('modificado')
+
+    modificacao.trim()
+    modificacao.innerText(dia);
+
+   
+}
